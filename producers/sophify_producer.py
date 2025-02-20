51aa9808b445cd85b8ce4aeafd542c3c865c8969
Priyankanaskar <pri.naha@gmail.com>
2025-02-19 21:21:23 -0600

    initial commit

--- producers/sophify_producer.py
+++ producers/sophify_producer.py
@@ -2,6 +2,8 @@ from kafka import KafkaProducer
 import json
 import time
 
+from consumers.shopify_Api import get_shopify_orders
+
 # Configure your Kafka producer
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
